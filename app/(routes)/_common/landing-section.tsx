12c91fa16746af0d4d2c68fd23ddc0f89fec961e
Ahmed <gamalelsadany2468@gmail.com>
2026-05-29 15:13:20 -0700

    add canvas editor and project features

--- app/(routes)/_common/landing-section.tsx
+++ app/(routes)/_common/landing-section.tsx
@@ -1,11 +1,26 @@
 "use client";
+import React, { memo, useState } from "react";
 import { Suggestion, Suggestions } from "@/components/ai-elements/suggestion";
 import PromptInput from "@/components/prompt-input";
-import React, { useState } from "react";
 import Header from "./Header";
+import { useCreateProject, useGetProjects } from "@/features/use-project";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { Spinner } from "@/components/ui/spinner";
+import { ProjectType } from "@/types/project";
+import { useRouter } from "next/navigation";
+import { FolderOpenDotIcon } from "lucide-react";
+import { formatDistanceToNow } from "date-fns";
 
 const LandingSection = () => {
+  const {user} = useKindeBrowserClient();
+
   const [promptText, setPromptText] = useState<string>("");
+  const userId = user?.id;
+
+  const {data: projects, isLoading, isError} = useGetProjects(userId);
+
+  const{mutate,isPending} = useCreateProject()
+
 
   const suggestions = [
     {
@@ -46,6 +61,11 @@ const LandingSection = () => {
     setPromptText(val);
   };
 
+  const handleSubmit= () =>{
+    if(!promptText)return;
+    mutate(promptText);
+  };
+
   return (
     <div className="w-full min-h-screen">
       <div className="flex flex-col">
@@ -74,8 +94,8 @@ const LandingSection = () => {
                   className="ring-2 ring-primary "
                   promptText={promptText}
                   setPromptText={setPromptText}
-                  isLoading={false}
-                  onSubmit={() => {}}
+                  isLoading={isPending}
+                  onSubmit={handleSubmit}
                 />
               </div>
 
@@ -106,18 +126,91 @@ const LandingSection = () => {
 
         <div className="w-full py-10">
           <div className="mx-auto max-w-3xl">
-            <div>
+            {userId && (
+               <div>
               <h1
                 className="font-medium text-xl tracking-tight"
               >
                 Recent Projects
               </h1>
+              {isLoading ? (
+                <div className="flex items-center justify-center py-2">
+                  <Spinner className="size-10" />
+                </div>
+              ):(
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 mt-3">
+                    {projects?.map((project:ProjectType) => (
+                      <ProjectCard key={project.id} project= {project}/>
+                    ))}
+                </div>
+              )
+            }
+
+
+
             </div>
+          )}
+           
+
+           {isError && <p className="text-red-500"> Failed to load projects</p>}
           </div>
         </div>
       </div>
     </div>
   );
 };
+const ProjectCard = memo(({ project }: { project: ProjectType }) => {
+  const router = useRouter();
 
+  const createdAtDate = new Date(project.createdAt);
+
+  const timeAgo = formatDistanceToNow(createdAtDate, {
+    addSuffix: true,
+  });
+
+  const thumbnail = project.thumbnail || null ;
+
+  const onRoute = () => {
+    router.push(`/project/${project.id}`);
+  };
+   
+    return <div 
+    role="button"
+    className="w-full flex flex-col border rounded-xl cursor-pointer
+    hover:shadow-md overflow-hidden"
+    onClick={onRoute}>
+      <div 
+      className="h-40 bg-[#eee] relative overflow-hidden
+      flex items-center justify-center"
+      >
+        {thumbnail ? (
+          <img
+          src={thumbnail}
+          className="w-full h-full object-over object-left scale-110"
+          />
+        ):(
+          <div 
+          className="w-16 h-16 rounded-full bg-primary/20
+          flex items-center justify-center text-primary"
+          >
+            <FolderOpenDotIcon />
+          </div>
+        )}
+      </div>
+
+        <div className="p-4 flex flex-col">
+          <h3 className="font-semibold text-sm 
+          truncate w-full mb-1 line-clamp-1">
+            {project.name}
+          </h3>
+          <p className="text-xs text-muted-foreground">
+            {timeAgo}
+          </p>
+
+        </div>
+
+    </div>
+});
+ProjectCard.displayName = "projectCard";
 export default LandingSection;
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Jost } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { QueryProvider } from "@/context/query-provider";
 
 const jostSans = Jost({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -22,7 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${jostSans.className} antialiased`}>
-        <ThemeProvider
+        <QueryProvider>
+           <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
@@ -31,6 +33,8 @@ export default function RootLayout({
           {children}
           <Toaster richColors position="bottom-center" />
         </ThemeProvider>
+        </QueryProvider>
+       
       </body>
     </html>
   );
